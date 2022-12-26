1be8f47c60b288cfc8c40af90dc9fa77f923cdeb
hotaruZzZz <108340230+hotaruZzZz@users.noreply.github.com>
2022-12-26 15:27:43 +0800

    Add files via upload

--- studen_suggest.py
+++ studen_suggest.py
@@ -1,19 +1,24 @@
 import studen_deep_linear
+#from . import studen_deep_linear
 import numpy as np
 import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 
 def suggest_start(new_data , n_days):
-    #pd.read_csv(data_name => 檔案名稱 ,usecols = [] => 選取指定列, nrows => 幾筆資料)
-    #tech_every_day3裡面目前有1943個不同ID，想查詢的話請找另外製作的ID表
+
     
-    mean5_data = new_data["mean5"]                    #"mean5"這個選項可以改成"open"
-    time_data = new_data["date"]
+    mean5_data = new_data[7]   #以收盤價作為訓練
+    time_data = new_data[3]    #日期
     X_X = mean5_data.values
+    X_X = np.asarray(X_X).astype('float32')
     np_time_data = time_data.values
+    np_time_data = np.asarray(np_time_data).astype('str')
+    for i in range(n_days):
+        np_time_data[i] = np_time_data[i][:10]
     number_time_data = []
     y_date = []
+    
     #將存在np_time_data裡面的字串時間轉換為datetime，以方便後續轉換數字
     for i in np_time_data:
         a = str(i)
@@ -37,15 +42,26 @@ def suggest_start(new_data , n_days):
     #資料標準化
     max_features, min_features = max(X_X), min(X_X)
     X_X = (X_X-min_features)/(max_features-min_features)
+    max_features, min_features = max(X_X), min(X_X)
+    X_X = (X_X-min_features)/(max_features-min_features)
     #資料三維處理
-    n = 30 #幾天為一筆
+    if n_days <= 30:
+        n = n_days - 10
+    elif n_days <= 10:
+        print("資料過少")
+        return 0
+    else:
+        n = 30
     X_train = []
     y_train = []
+#print(n, n_days)
     for i in range(n, n_days):
+        #print(1)
         X_train.append(X_X[i-n:i, 0])
         y_train.append(X_X[i, 0])
     X_train, y_train = np.array(X_train), np.array(y_train)
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    
     #開始訓練
     model , pred = studen_deep_linear.studen_CNN_LSTM_model(
         X_train , y_train, epochs = 6 , batch_size = 20 , learning_rate = 0.001
@@ -54,7 +70,7 @@ def suggest_start(new_data , n_days):
     new_time_data = np_time_data
     for i in range(31):
         time_str = new_time_data[len(new_time_data)-1]
-        da = int(time_str[8:])
+        da = int(time_str[8:10])
         mo = int(time_str[5:7])
         Ya = int(time_str[:4])
         new_data = studen_deep_linear.create_data(Ya , mo , da)
@@ -63,22 +79,29 @@ def suggest_start(new_data , n_days):
     X_X = X_X * (max_features-min_features) + min_features
     pred = pred * (max_features-min_features) + min_features
     X_pred = X_pred * (max_features-min_features) + min_features
+    
     #繪製
     #draw_data(第一個X軸，第一個Y軸，第二個X軸，第二個Y軸，上下間距，範圍開啟(0為關閉，其他數字都是開啟)，標題)
-    studen_deep_linear.draw_data(np_time_data , X_X, new_time_data, X_pred, n, axis = 1 , name = 'data_predict')
-    studen_deep_linear.draw_data(np_time_data , X_X, np_time_data[n:] , pred, n, axis = 0 , name = 'data_learing')
+    #studen_deep_linear.draw_data(np_time_data , X_X, new_time_data, X_pred, n, axis = 1 , name = 'data_predict')
+    #studen_deep_linear.draw_data(np_time_data , X_X, np_time_data[n:] , pred, n, axis = 0 , name = 'data_learning')
+    
+    #print(studen_deep_linear.draw_plotly(np_time_data , X_X, np_time_data[30:] , pred, 30, axis = 0 , name = 'plotly'))
     #draw_data_for_date(np_time_data , X_X , 30)
     #Y_pred[len(Y_pred)-n]+3
+    plotly_div_result = studen_deep_linear.draw_plotly(np_time_data , X_X, new_time_data, X_pred, n, axis = 1 , name = '預測結果')
+    plotly_div = studen_deep_linear.draw_plotly(np_time_data , X_X, np_time_data[30:] , pred, 30, axis = 0 , name = '訓練結果')
+    
     if X_pred[len(X_pred)-1] <= X_X[len(X_X)-1]:
         suggest = 0
     else:
         suggest = 1
     if __name__ == "__main__":
-        return suggest , X_pred , pred , np_time_data , X_X , Y_Y
+        return suggest , X_pred , pred , np_time_data , X_X , new_time_data
     else:
-        number = X_X[len(X_X)-1] - X_pred[len(X_pred)-1]
-        return suggest , number
+        number = X_pred[len(X_pred)-1]
+        return suggest , number, plotly_div, plotly_div_result 
     
+      
 if __name__ == "__main__":
     data = (pd.read_csv("tech_every_day3.csv")
            [lambda x: x['stock_id'] == 57 ]     #lambda x:x*x為密名函示 , 類似 def a(x): return x*x
@@ -87,5 +110,6 @@ if __name__ == "__main__":
     n_days = len(new_data)  
     suggest , X_pred , pred , np_time_data , X_X , Y_Y = suggest_start(new_data , n_days)
     n = 30
-    print_x , print_y , print_new_X , print_new_Y = studen_deep_linear.draw_plotly(np_time_data , X_X, np_time_data[30:] , pred, 30, axis = 0 , name = 'plotly')
+    print(studen_deep_linear.draw_plotly(np_time_data , X_X, np_time_data[30:] , pred, 30, axis = 0 , name = 'plotly'))
+    #print_x , print_y , print_new_X , print_new_Y = studen_deep_linear.draw_plotly(np_time_data , X_X, np_time_data[30:] , pred, 30, axis = 0 , name = 'plotly')
     
