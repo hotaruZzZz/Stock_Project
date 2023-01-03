7202c852b32e9cdf06d454a64dfb6b7ba7133eff
New-martin <109015619+New-martin@users.noreply.github.com>
2023-01-03 23:30:49 +0800

    Update new.py

--- new.py
+++ new.py
@@ -29,89 +29,100 @@ db.close()
 list_id = list_id.values
 print(list_id)
 print(type(list_id))
-tabel_confirm = [0] * len(list_id)
-suggest_total = 0
-'''
-for i in suggest_id:
-    print(i[0])
-'''
-def fun(db , cursor , tabel_confirm , suggest_total):
-    import random
-    suggest_id = []
-    suggest_long = 0
-    while suggest_long < 20:
-        random_number = random.randrange(22748)
-        if tabel_confirm[random_number] == 0:
-            suggest_id.append(list_id[random_number])
-            tabel_confirm[random_number] = 1
-            suggest_long += 1
-            suggest_total += 1
-        if suggest_list == 22748:
-            return 0,0,tabel_confirm,0
-    first = ['0050']
-    suggest_id.insert(0 , np.array(first))
-    b = 0
-    first_number = 0
-    number_list = []
-    for i in suggest_id:         #range(10)改成list_id
-        data = []
-    
-        db = pymysql.connect(host='localhost', port=3306, user='test', passwd='1234123zxc', db='django', charset='utf8')
-        #建立操作游標
-        cursor = db.cursor()
-        #SQL語法
-        sql = (
-            "select * from stock_data_data where sDate > '2020-11-01' and sNumber = '"
-            + str(i[0]) +
-            "' and sClose != (-1.00) order by sDate;"
-            )#執行語法
-        print(sql)
-        try:
-          cursor.execute(sql)
-          result = DataFrame(cursor.fetchall())
-          data = result
-          if data.empty == True:
-              #print('empty')
-              pass
-          else:
-              #提交修改
-              db.commit()
-              #print(data)
-              print('success')
-        except:
-          #發生錯誤時停止執行SQL
-          db.rollback()
-          print('error')
-          
-        #print(data)#close = np.array(data)
-        #close = data[7]
-        if b == 0:
-            first_number = studen_suggest.suggest_start(data, len(data) , b)
-            b = 1
-        else:
-            suggest , number, plotly, plotly_2 = studen_suggest.suggest_start(data, len(data) , b)
-            number_list.append((str(i[0]) , number[0]))
-    #關閉連線
-    db.close()
-    return first_number[0], number_list, tabel_confirm, suggest_total
+suggest_id = ['2603'
+                ,'3034'
+                ,'2454'
+                ,'2382'
+                ,'2357'
+                ,'2409'
+                ,'4938'
+                ,'3711'
+                ,'2324'
+                ,'2347'
+                ,'2301'
+                ,'1102'
+                ,'2303'
+                ,'1402'
+                ,'3231'
+                ,'2356'
+                ,'3702'
+                ,'2891'
+                ,'2379'
+                ,'2377'
+                ,'2027'
+                ,'8046'
+                ,'2890'
+                ,'2317'
+                ,'2885'
+                ,'2474'
+                ,'1301'
+                ,'1303'
+                ,'4958'
+                ,'2376'
+                ,'2385'
+                ,'2337'
+                ,'6239'
+                ,'2383'
+                ,'1802'
+                ,'2915'
+                ,'3036'
+                ,'2542'
+                ,'3044'
+                ,'6176'
+                ,'3189'
+                ,'2637'
+                ,'2449'
+                ,'5522'
+                ,'2606'
+                ,'3017'
+                ,'2006'
+                ,'2809'
+                ,'3665'
+                ,'6271']
+number_list = []
+for i in suggest_id:         #range(10)改成list_id
+    data = []
+
+    db = pymysql.connect(host='localhost', port=3306, user='test', passwd='1234123zxc', db='django', charset='utf8')
+    #建立操作游標
+    cursor = db.cursor()
+    #SQL語法
+    sql = (
+        "select * from stock_data_data where sDate > '2020-11-01' and sNumber = '"
+        + str(i[0]) +
+        "' and sClose != (-1.00) order by sDate;"
+        )#執行語法
+    print(sql)
+    try:
+      cursor.execute(sql)
+      result = DataFrame(cursor.fetchall())
+      data = result
+      if data.empty == True:
+          #print('empty')
+          pass
+      else:
+          #提交修改
+          db.commit()
+          #print(data)
+          print('success')
+    except:
+      #發生錯誤時停止執行SQL
+      db.rollback()
+      print('error')
+      
+    #print(data)#close = np.array(data)
+    #close = data[7]
+    suggest , number, plotly, plotly_2 = studen_suggest.suggest_start(data, len(data) , 1)
+    number_list.append((str(i[0]) , number[0]))
+#關閉連線
+db.close()
+max_number = 0
+max_id = '0'
 suggest_list = []
-BREAK = 1
-total = 1
-while BREAK:
-    if len(suggest_list) == 0:
-        first_number , number_list, tabel_confirm, suggest_total = fun(db , cursor , tabel_confirm , suggest_total)
-        if first_number == 0 and number_list == 0 and suggest_total == 0:
-            print("無法找到更好的")
-            break
-        number_suggest = 0
-        
-        print(type(first_number) , first_number)
-        print('-----------%d sugggest---------------'%total)
-        total += 1
-        for i in number_list:
-            #print(type(i[0]) , i[0])
-            if first_number < i[1]:
-                suggest_list.append(i[0])
-    else:
-        print("suggest:\n" , suggest_list)
-        break
+number_suggest = 0
+for i in number_list:
+    #print(type(i[0]) , i[0])
+    if max_number < i[1]:
+        max_number = i[1]
+        max_id = i[0]     
+print("suggest:\n------" ,max_id,'------\n')
