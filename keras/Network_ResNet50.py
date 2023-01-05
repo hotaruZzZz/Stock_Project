21c55e2448f0fcc5b88df0920d0a41fa0e38dd1d
New-martin <109015619+New-martin@users.noreply.github.com>
2023-01-05 22:34:24 +0800

    Add files via upload

---
+++ keras/Network_ResNet50.py
@@ -0,0 +1,125 @@
+import cv2
+import numpy as np
+import os
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as K
+import keras
+from keras.models import Sequential, Model,load_model
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D
+from keras.preprocessing import image
+from keras.initializers import glorot_uniform
+from keras.optimizers import Adam
+
+def identity_block(X, f, filters, stage, block):
+   
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    F1, F2, F3 = filters
+
+    X_shortcut = X
+   
+    X = Conv2D(filters=F1, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2a', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+    X = Add()([X, X_shortcut])# SKIP Connection
+    X = Activation('relu')(X)
+
+    return X
+
+def convolutional_block(X, f, filters, stage, block, s=2):
+   
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    F1, F2, F3 = filters
+
+    X_shortcut = X
+
+    X = Conv2D(filters=F1, kernel_size=(1, 1), strides=(s, s), padding='valid', name=conv_name_base + '2a', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+    X_shortcut = Conv2D(filters=F3, kernel_size=(1, 1), strides=(s, s), padding='valid', name=conv_name_base + '1', kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(axis=3, name=bn_name_base + '1')(X_shortcut)
+
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    return X
+
+def studen_creat_ResNet50(input_shape=(224, 224, 3)):
+    X_input = Input(input_shape)
+
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    X = Conv2D(64, (7, 7), strides=(2, 2), name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name='bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    X = convolutional_block(X, f=3, filters=[64, 64, 256], stage=2, block='a', s=1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+
+    X = convolutional_block(X, f=3, filters=[128, 128, 512], stage=3, block='a', s=2)
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+    X = convolutional_block(X, f=3, filters=[256, 256, 1024], stage=4, block='a', s=2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    X = convolutional_block(X, f=3, filters=[512, 512, 2048], stage=5, block='a', s=2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+    X = AveragePooling2D(pool_size=(2, 2), padding='same')(X)
+    
+    base_model = Model(inputs=X_input, outputs=X, name='ResNet50')
+    x_model = base_model.output
+    x_model = Flatten()(x_model)
+    x_model = Dense(256, activation='relu', name='fc1',kernel_initializer=glorot_uniform(seed=0))(x_model)
+    x_model = Dense(128, activation='relu', name='fc2',kernel_initializer=glorot_uniform(seed=0))(x_model)
+    x_model = Dense(64, activation='relu', name='fc4',kernel_initializer=glorot_uniform(seed=0))(x_model)
+    output = Dense(11,activation='softmax', name='fc5',kernel_initializer=glorot_uniform(seed=0))(x_model)
+    model = Model(inputs = base_model.input , outputs = output)
+    return model
+
+def application_model(train_generator,test_generator , INPUT = (224,224,3)):
+    ba_model = studen_creat_ResNet50(INPUT,)
+    headModel = ba_model.output
+    headModel = Flatten()(headModel)
+    output = Dense(5,activation='sigmoid', name='fc5',kernel_initializer=glorot_uniform(seed=0))(headModel)
+    model = Model(inputs=ba_model.input, outputs=headModel)
+    model.compile(
+        optimizer=Adam(0.001), loss="categorical_crossentropy", metrics=["accuracy"]
+    )
+    model.load_weights("resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5")
+    for layer in model.layers:
+        layer.trainable = False
+    es=EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=20)
+    mc = ModelCheckpoint('/content/gdrive/My Drive/best_model.h5', monitor='val_accuracy', mode='')
+    output = model.fit_generator(train_generator,validation_data=test_generator,epochs=100,verbose=1,callbacks=[mc,es])
+    return model , output
