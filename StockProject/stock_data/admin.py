9816d501e7ed8f6cb6f4389367318921dff8ef5a
hotaruZzZz <108340230+hotaruZzZz@users.noreply.github.com>
2023-01-09 23:30:37 +0800

    Delete StockProject directory

--- StockProject/stock_data/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Data, DataAdmin
-
-
-# Register your models here.
-
-#admin.site.register(Data, DataAdmin)
--- StockProject/stock_data/filters.py
+++
@@ -1,9 +0,0 @@
-from .models import Data
-import django_filters
-
-class DataFilter(django_filters.FilterSet):
-
-
-    class Meta:
-        model = Data
-        fields = '__all__'
